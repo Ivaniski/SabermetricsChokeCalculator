d1781d0a230c2d2cef61020eaf75a07cbdb620f5
Ivaniski <34961876+Ivaniski@users.noreply.github.com>
2018-01-06 00:27:56 -0800

    Updated functionality and appearance

--- app/src/main/java/gg/cannibalpudge/sabermetricschokecalculator/MainActivity.java
+++ app/src/main/java/gg/cannibalpudge/sabermetricschokecalculator/MainActivity.java
@@ -3,15 +3,15 @@ package gg.cannibalpudge.sabermetricschokecalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.lang.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static final String EXTRA_MESSAGE = "gg.cannibalpudge.sabermetricschokecalculator";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,20 +19,105 @@ public class MainActivity extends AppCompatActivity {
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String output = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView7);
-        textView.setText(message);
+        textView.setText(output);
+
+
     }
 
-    /** Called when the user taps the Send button */
+    /* Called when the user taps the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, MainActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText2);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+
+    //---------------------------read input-----------------------------//
+
+        EditText editTextMine1 = (EditText) findViewById(R.id.editText2);
+        String message1 = editTextMine1.getText().toString();
+
+        EditText editTextMine2 = (EditText) findViewById(R.id.editText3);
+        String message2 = editTextMine2.getText().toString();
+
+        EditText editTextMine3 = (EditText) findViewById(R.id.editText4);
+        String message3 = editTextMine3.getText().toString();
+
+        EditText editTextMine4 = (EditText) findViewById(R.id.editText5);
+        String message4 = editTextMine4.getText().toString();
+
+    //---------------------truncate string -> int-----------------------//
+//
+
+
+        checkInput(message1);
+
+//        if(message2 == null) {
+//            message2 = 0;
+//        }else{
+//            return message2;
+//        }
+//
+//        if(message3 == null) {
+//            message3 = 0;
+//        }else{
+//            return message3;
+//        }
+//
+//        if(message4 == null) {
+//            message4 = 0;
+//        }else{
+//            return message4;
+//        }
+//
+        String number1 = message1;
+        int result1 = Integer.parseInt(number1);
+
+        String number2 = message2;
+        int result2 = Integer.parseInt(number2);
+
+        String number3 = message3;
+        int result3 = Integer.parseInt(number3);
+
+        String number4 = message4;
+        int result4 = Integer.parseInt(number4);
+
+    //---------------------------sabermetrics-----------------------------//
+
+
+
+        int answer = result1 + result2 + result3 + result4;
+
+    //----------------------truncate int -> string------------------------//
+
+        String calculation = Integer.toString(answer);
+
+    //------------------------------output--------------------------------//
+
+
+        intent.putExtra(EXTRA_MESSAGE, calculation);
         startActivity(intent);
+
+    }
+
+
+//    private boolean isEmpty(EditText etText) {
+//        if (etText.getText().toString().trim().length() > 0)
+//            return false;
+//
+//        return true;
+//    }
+
+    String checkInput(String input){
+        if(input != "") {
+            return input;
+        }else{
+            String number = input;
+            int result = Integer.parseInt(number);
+            result = 0;
+            String output = Integer.toString(result);
+            return output;
+        }
     }
 }
 
